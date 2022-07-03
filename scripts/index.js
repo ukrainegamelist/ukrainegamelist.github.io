2c1d163014b9dc3ba97908745be56f0ff1316d47
ukrainegamelist <qqwqweqwerqwerty9@gmail.com>
2022-07-03 23:03:58 +0200

    fix 7 js

--- scripts/index.js
+++ scripts/index.js
@@ -5,13 +5,17 @@ const gameList =[
 function showGameList()
 {
 	const feed = document.getElementById("feed");
-
+	if(feed==null){
+		alert("cant find feed");
+	}
 	for(let i=0; i<gameList.length; i++){
 		var p = document.createElement("p");
 		var text = document.createTextNode(gameList[i]);
 		p.appendChild(text);
 		feed.appendChild(p);
+		alert("executed loop");
 	}
+	alert("function showGameList() finished");
 }
 
 document.onload = showGameList;
