cfb7f3e6fa453cc7fe0d9df83f57f6d06e93d9b6
ukrainegamelist <qqwqweqwerqwerty9@gmail.com>
2022-07-03 23:38:02 +0200

    fix js 10

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,19 @@
 const gameList =[
-"gra 1","gra 2"
+"STONKS-9800: Stock Market Simulator","Screaming Noppy"
 ];
 
 function showGameList()
 {
 	const feed = document.getElementById("feed");
 	if(feed==null){
-		alert("cant find feed");
+		alert("error:cant find id:feed");
 	}
 	for(let i=0; i<gameList.length; i++){
 		var p = document.createElement("p");
 		var text = document.createTextNode(gameList[i]);
 		p.appendChild(text);
 		feed.appendChild(p);
-		alert("executed loop");
 	}
-	alert("function showGameList() finished");
 }
 
 document.addEventListener("DOMContentLoaded", showGameList); 
-
-alert("script is working!");
