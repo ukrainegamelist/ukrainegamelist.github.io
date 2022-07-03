869f9e6aceb6be02dd2cb5669e21e2d1c034e6d9
ukrainegamelist <qqwqweqwerqwerty9@gmail.com>
2022-07-03 22:29:38 +0200

    added js script

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,14 @@
-alert(location.hostname);
+const gameList =[
+[
+"gra 1","gra 2"
+];
+
+const feed = document.getElementById("feed");
+
+for(let i=0; i<feed.length; i++){
+	let p=document.createElement("p");
+	p.textContent=feed[i];
+	feed.appendChild(p);
+}
+
+alert("script is working!");
