30a473b97f455e6a766c918805b612b9a737be94
ukrainegamelist <qqwqweqwerqwerty9@gmail.com>
2022-07-07 21:35:53 +0200

    Added game list

--- scripts/index.js
+++ scripts/index.js
@@ -2,18 +2,45 @@ const gameList =[
 "STONKS-9800: Stock Market Simulator","Screaming Noppy"
 ];
 
+
+
+function pickLocalLanguage()
+{
+	const languages = navigator.languages;
+	for(let i=0; i<languages.length; i++){
+	        if(languages[i]=="uk-UA"){
+	                const maintitle = document.getElementById("maintitle");
+			if(maintitle==null){
+				alert("&#&");
+			}
+			maintitle.innerHTML = "Українські інді-ігри";
+	        }
+	}
+}
+
+
 function showGameList()
 {
+	const jsonGameListFile = require('../games/uagames.json');
+	const jsonGameList = JSON.parse(jsonGameListFile);
+
 	const feed = document.getElementById("feed");
 	if(feed==null){
 		alert("error:cant find id:feed");
 	}
-	for(let i=0; i<gameList.length; i++){
-		var p = document.createElement("p");
-		var text = document.createTextNode(gameList[i]);
+	for(let i=0; i<jsonGameList.gameList.length; i++){
+		var p = document.createElement("p")
+		var jsonGame = jsonGameList.gameList[i];
+		var text = document.createTextNode(jsonGame.name);
 		p.appendChild(text);
 		feed.appendChild(p);
 	}
 }
 
-document.addEventListener("DOMContentLoaded", showGameList); 
+function main()
+{
+	pickLocalLanguage();
+	showGameList();
+}
+
+document.addEventListener("DOMContentLoaded", main);
